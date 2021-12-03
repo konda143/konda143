5c63239c9047449c977f985dbc4c8f66d4d7dfa4
kondareddy <kondarockc@gmail.com>
2021-12-03 22:02:44 +0530

    acessing is third commit
     branch main
    
    Your branch is ahead of 'origin/main' by 1 commit.

---
+++ accessing.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a[10],i;
+	for(i=0;i<=10;i++)
+	{
+		printf("enter the elements a[%d]\n",i);
+		scanf("%d",&a[i]);
+}
+printf("the elements are");
+for(i=0;i<=10;i++)
+{
+	printf("%d",a[i]);
+}
+}
+
